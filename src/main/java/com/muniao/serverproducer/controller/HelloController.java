bd5dd7f0281cea37e28eb8bd4040477ae2253500
lixp <846358830@qq.com>
2019-07-24 13:38:35 +0800

    服务提供 加入配置中心

--- src/main/java/com/muniao/serverproducer/controller/HelloController.java
+++ src/main/java/com/muniao/serverproducer/controller/HelloController.java
@@ -2,12 +2,15 @@ package com.muniao.serverproducer.controller;
 
 import com.muniao.serverproducer.vo.UserVO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//这里面的属性有可能会更新的，git中的配置中心变化的话就要刷新，没有这个注解内，配置就不能及时更新
+@RefreshScope
 public class HelloController
 {
     @Value("${server.port}")
@@ -28,6 +31,14 @@ public class HelloController
     {
         System.out.println(user);
         return "hello name:"+user.getUserName()+" age:"+user.getAge()+" birthday:"+user.getBirthday()+"，serverPort:"+serverPort;
+    }
+
+    @Value("${hello.msg}")
+    private String helloMsg;
+
+    @RequestMapping("/helloConfig")
+    public String helloConfig() {
+        return this.helloMsg;
     }
 
 }
